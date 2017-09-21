20c7e7a95f1dec9b80a13f3beb6236b6843e614b
Unknown <hawkest14@gmail.com>
2017-09-20 20:15:23 -0600

    DGM_1600 9/20

---
+++ DGM1600_Game/Assets/Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+	public string[] heros = new string[6];
+
+
+	// Use this for initialization
+	void Start () {
+	heros[0] = "Link";
+	heros[1] = "Gibbs";
+	heros[2] = "Sgt. Johnson";
+	heros[3] = "Master Chief";
+	heros[4] = "Ezio";
+	heros[5] = "soap";
+	
+	print(heros[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
